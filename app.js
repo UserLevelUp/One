fc4b99b4c5743646e9ff27d6b3789f99db204836
Marc Noon <marcnoon@msn.com>
2017-12-10 13:01:25 -0800

    Added scope.  Updated app.js to retrieve index.html and game.js files.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.get("/", function(req, res){
-    console.log("started / call");
-    res.send("<h1>Hello world mn</h1>");
+    // console.log("started / call");
+    // res.send("<h1>Hello world mn</h1>");
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get("/game.js", function(req, res){
+    res.sendFile(path.join(__dirname + '/game.js'));
 });
 
 app.listen(3000, function(error) {
--- game.js
+++ game.js
@@ -1,8 +1,35 @@
+var scope = {};  // current scope.  i.e. which game are we currently interested in
 var game = {
-    init: function() {
 
+    // using wikipedia for reference and images 
+    // https://en.wikipedia.org/wiki/Uno_(card_game)#/media/File:UNO_cards_deck.svg
+
+    scope: {
+        'instanceName': null, // when this game is created give it a name
+        'uniqueIdentifier': null,
+    }, // this initializes all the game objects and adds players who wish to join
+    init: function() {
+        let initSteps =
+        {
+            initCards: function() {
+               // add 0 - 9 cards to start with
+               scope.colors.forEach(color => {
+               for(let i=0;i<10;i++)
+               {
+                    scope.addCard(i.toString(10), color, i);
+               } 
+            });
+            
+            }
+                
+        };
+        initSteps.initCards(); // perform the first step
     },
     addPlayer: function(name)
+    {
+
+    },
+    addCard: function(name, color, points)
     {
 
     },
@@ -12,10 +39,35 @@ var game = {
     deal: function() { 
         // deal the deck
     },
-    player: [],
-    cards: [],
+    player: [], // placeholder to initialize new players who want to join game
+    colors: ['red', 'yellow', 'green', 'blue'],
+    speicalCardsWithZero: [     {'type': 'skip', 'points': 20}, 
+                                {'type': 'reverse', 'points': 20},
+                                {'type': 'plus2', 'points': 20},
+                                {'type': 'wild', 'points': 50}
+                             ],
+    speicalCardsWithoutZero: [  {'type': 'skip', 'points': 20}, 
+                                {'type': 'reverse', 'points': 20},
+                                {'type': 'plus2', 'points': 20},
+                                {'type': 'draw4', 'points': 50}
+                             ],
+    cards: [], // place holder to initialize and use cards
     gameModes: ["initializing", "shufling", "dealing", "player_turn", "player_action"],
     mode: null,
+    gameLoop: function()
+    {
+        scope = this; // this game
+        while(scope.continueGame)
+        {
+           scope.init();  // initialize the game ... see init function above
+            // actual game play goes here somewhere...
+            /// ...
 
-
+           // for now lets immediately end the game loop
+           scope.continueGame = false;
+        }        
+    },
+    continueGame: true, // currently set to false
 };
+
+game.gameLoop(); // begin the game loop
